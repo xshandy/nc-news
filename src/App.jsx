b4de911d08fb558b6df4dff55a3f476a51a2c024
xshandy <shandygshek@hotmail.co.uk>
2025-02-17 17:53:23 +0000

    amend based on feedbacks-remove searchbar as backend does not support it and moved article state to ArticleList.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,18 @@
 import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router";
-import { fetchArticles } from "./server";
+
 import Header from "./Components/Header";
 import ArticleList from "./Components/ArticleList";
 
 import "./App.css";
+import NavBar from "./Components/NavBar";
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setLoading(true);
-    fetchArticles(searchTerm).then((data) => {
-      setArticles(data);
-      setLoading(false);
-    });
-  }, [searchTerm]);
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
   return (
     <>
-      <Header setSearchTerm={setSearchTerm} />
-      <ArticleList articles={articles} />
+      <NavBar />
+      <Header />
+      <ArticleList />
     </>
   );
 }
--- src/Components/ArticleList.jsx
+++ src/Components/ArticleList.jsx
@@ -1,9 +1,26 @@
-function ArticleList({ articles }) {
+import { useState, useEffect } from "react";
+import { fetchArticles } from "../server";
+
+function ArticleList() {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    fetchArticles().then((data) => {
+      setArticles(data);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className="articleList-container">
       <ul>
         {articles.map((article) => {
-          console.log(article);
           return (
             <li key={article.article_id}>
               <h3>{article.title}</h3>
